e3c9cf0669fee4f3f3e815a6521699e24b259a62
cuddiale <alessiocuddretto@hotmail.com>
2024-05-28 09:32:19 +0200

    Update

--- script.js
+++ script.js
@@ -68,6 +68,10 @@ function calculateCalories(e) {
 
   const consumedCalories =
     breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories =
+    budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? "Surplus" : "Deficit";
+  output.innerHTML = ``;
 }
 
 function getCaloriesFromInputs(list) {
